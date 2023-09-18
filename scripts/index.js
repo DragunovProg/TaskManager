dbc80619a06dd32b8e5126cadba0812f9dae1516
Igor Dragunov <igor.dragunov99@gmail.com>
2023-09-18 17:39:36 +0300

    add task handle and checkbox complete button and  delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,47 @@
-const taskListDiv = document.querySelector('.task-list');
+const goalText = document.querySelector('.goal-text');
+const taskList = document.querySelector('.task-list');
+
+
 
 document.querySelector('.btn-add').addEventListener('click', ()=> {
-    taskListDiv.innerHTML = "<div class=\"task-title\">Goal 1</div><input type=\"checkbox\" name=\"tast-complete\" id=\"task-complete-checkbox\"></div>";
+    if(goalText.value != '') {
+        console.log(goalText.value);
+        const taskElement = document.createElement('div');
+        const taskTitle = document.createElement('div');
+        const completeCheckbox = document.createElement('input');
+        const deleteButton = document.createElement('button');
+
+
+
+        taskElement.classList.add('task-element');
+        taskTitle.classList.add('task-title')
+        
+        completeCheckbox.id = 'checkbox-complete';
+        completeCheckbox.type = 'checkbox';
+        
+        
+        deleteButton.classList.add('btn-del');
+        deleteButton.innerText = "Delete"
+        
+
+        taskElement.appendChild(taskTitle);
+        taskElement.appendChild(completeCheckbox);
+        taskElement.appendChild(deleteButton);
+
+        deleteButton.onclick = (event)=> {
+            event.currentTarget.closest('.task-element').remove();
+        }
+
+        completeCheckbox.onchange = (event)=> {
+            if(event.currentTarget.checked) {
+                taskTitle.style.textDecoration = 'line-through';
+            } else {
+                taskTitle.style.textDecoration = 'none';
+
+            }
+        }
+
+        taskTitle.innerText = goalText.value;
+        taskList.appendChild(taskElement);
+    }
 })
