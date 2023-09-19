d2f1e814cbceb8adbec2f1bd6a4b68b16afc91c7
Igor Dragunov <igor.dragunov99@gmail.com>
2023-09-19 10:30:08 +0300

    add goal-text clearing

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,50 @@
 const goalText = document.querySelector('.goal-text');
 const taskList = document.querySelector('.task-list');
+const buttonAdd = document.querySelector('.btn-add');
+const completeCheckboxes = document.querySelectorAll('.checkbox-complete');
+const deleteButtons = document.querySelectorAll('btn-del');
+
+buttonAdd.onclick = addTask;
 
 
 
-document.querySelector('.btn-add').addEventListener('click', ()=> {
+function addTask() {
     if(goalText.value != '') {
-        console.log(goalText.value);
         const taskElement = document.createElement('div');
         const taskTitle = document.createElement('div');
         const completeCheckbox = document.createElement('input');
         const deleteButton = document.createElement('button');
 
-
-
         taskElement.classList.add('task-element');
         taskTitle.classList.add('task-title')
         
-        completeCheckbox.id = 'checkbox-complete';
+        completeCheckbox.classList.add('checkbox-complete');
         completeCheckbox.type = 'checkbox';
-        
-        
+
+        completeCheckbox.onchange = (event) => {
+            if(event.currentTarget.checked) {
+                taskTitle.style.textDecoration = 'line-through';
+            } else {
+                taskTitle.style.textDecoration = 'none';
+        }
+        };
+
         deleteButton.classList.add('btn-del');
         deleteButton.innerText = "Delete"
-        
-
-        taskElement.appendChild(taskTitle);
-        taskElement.appendChild(completeCheckbox);
-        taskElement.appendChild(deleteButton);
 
         deleteButton.onclick = (event)=> {
             event.currentTarget.closest('.task-element').remove();
         }
 
-        completeCheckbox.onchange = (event)=> {
-            if(event.currentTarget.checked) {
-                taskTitle.style.textDecoration = 'line-through';
-            } else {
-                taskTitle.style.textDecoration = 'none';
-
-            }
-        }
+        taskElement.appendChild(taskTitle);
+        taskElement.appendChild(completeCheckbox);
+        taskElement.appendChild(deleteButton);
 
         taskTitle.innerText = goalText.value;
         taskList.appendChild(taskElement);
+        goalText.value = '';
     }
-})
+}
+
+
+
